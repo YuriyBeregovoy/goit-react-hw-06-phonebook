a66fc62af07a167f83448cbef7e440041d854200
YuriyBeregovoy <rik0681@gmail.com>
2023-09-19 22:26:49 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,7 +4,7 @@ import { List } from "./ContactList.styled"
 export const ContactList = ({contacts, onContactDelete}) => {
   return <List>
     {contacts.map(contact => (
-      <ContactListItem contact={contact} onContactDelete={onContactDelete} />
+      <ContactListItem  key={contact.id} contact={contact} onContactDelete={onContactDelete} />
           ))}
         </List>
 }
