11b321dc32ea2fd3acf082276a466e016a8264a7
YuriyBeregovoy <rik0681@gmail.com>
2023-09-20 17:21:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/GlobalStyle';
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <Provider store={store}>
       <App />
-      <GlobalStyle/>
-   
-  </React.StrictMode>
+      <GlobalStyle />
+    </Provider>
+     </React.StrictMode>
 );
