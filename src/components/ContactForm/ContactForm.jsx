0b020c21dfd6209bcfefe9d3a5be0e9119902d8f
YuriyBeregovoy <rik0681@gmail.com>
2023-09-21 18:35:03 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,18 +1,34 @@
 import { FormButton, FormContacts, LabelContacts } from "./ContactForm.styled"
+import { useDispatch } from "react-redux";
+import { addContacts } from "redux/contactsSlice";
+import { nanoid } from 'nanoid'
 
-export const ContactForm = ({addContactName, setNanoidId, onChangeName, onChangeNumber, nameInpytValue, numberIputValue }) => {
-  return  <FormContacts onSubmit={addContactName}>
-      <LabelContacts htmlFor={setNanoidId}>
+
+export const ContactForm = () => {
+
+  const dispatch = useDispatch();
+
+   const handleNameSet = e => {
+    e.preventDefault();
+     const form = e.target;
+     const name = form.elements.name.value;
+     const number = form.elements.number.value;
+    dispatch(addContacts({ name, number }));
+    form.reset();
+    
+  }
+  
+
+  return  <FormContacts onSubmit={handleNameSet}>
+      <LabelContacts htmlFor={nanoid()}>
           <span>Name</span>
           <input
-        id={setNanoidId}
+   
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-        value={nameInpytValue} 
-        onChange={onChangeName}
+       
           />
         </LabelContacts>
         <LabelContacts>
@@ -23,9 +39,7 @@ export const ContactForm = ({addContactName, setNanoidId, onChangeName, onChange
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
-        value={numberIputValue} 
-        onChange={onChangeNumber}
-          />
+                />
         </LabelContacts>
        <FormButton type="submit">Add contact</FormButton>
       </FormContacts>
