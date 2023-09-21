10b8de4721eff6e514fa49806631b8b4df783bc7
YuriyBeregovoy <rik0681@gmail.com>
2023-09-21 14:31:48 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { createAction, createReducer } from '@reduxjs/toolkit'
 
+export const action = createAction();
+export const reducer = createReducer();
+
 export const store = configureStore({
   reducer: {},
 });
