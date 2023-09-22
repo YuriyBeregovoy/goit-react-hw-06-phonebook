e190a48d2092d02956a914c977ea34916202109d
YuriyBeregovoy <rik0681@gmail.com>
2023-09-22 15:58:02 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { contactsReducer } from "./contactsSlice"
 import {filterReducer} from "./filterSlice"
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'localforage';
 
+const persistConfig = {
+key: 'root',
+ storage,
+};
 
+const rootReducer = {
+  filter: filterReducer,
+  contacts: contactsReducer,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    filter: filterReducer,
-    contacts: contactsReducer,
-  },
+  reducer: persistedReducer, 
 });
+
+export const persistor = persistStore(store);
