eb632ae02bdf28d41135669081f4807c87bd180c
YuriyBeregovoy <rik0681@gmail.com>
2023-09-22 19:00:37 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,45 +1,71 @@
-import { FormButton, FormContacts, LabelContacts } from "./ContactForm.styled"
+import React, { useState } from "react";
+import { FormButton, FormContacts, LabelContacts } from "./ContactForm.styled";
 import { useDispatch } from "react-redux";
 import { addContacts } from "redux/contactsSlice";
 import { nanoid } from "@reduxjs/toolkit";
+import { store } from "redux/store";
 
 
 export const ContactForm = () => {
-
   const dispatch = useDispatch();
+  const [formData, setFormData] = useState({ name: "", number: "" });
 
-   const handleNameSet = e => {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-     const form = e.target;
-     const name = form.elements.name.value;
-     const number = form.elements.number.value;
-    dispatch(addContacts({ name, number }));
-    form.reset();
-      }
-  
 
-  return  <FormContacts onSubmit={handleNameSet}>
-      <LabelContacts htmlFor={nanoid()}>
-          <span>Name</span>
-          <input
-        id = {nanoid()} 
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-       
-          />
-        </LabelContacts>
-        <LabelContacts>
-          <span>Number</span>
-          <input
-        type="tel"
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-                />
-        </LabelContacts>
-       <FormButton type="submit">Add contact</FormButton>
-      </FormContacts>
-}
+    if (formData.name.trim() === "" || formData.number.trim() === "") {
+      
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      ...formData,
+    };
+
+    dispatch(addContacts(newContact));
+
+  
+    setFormData({ name: "", number: "" });
+
+
+const currentState = store.getState();
+console.log("Поточний стан сховища Redux:", currentState);
+
+  };
+
+  return (
+    <FormContacts onSubmit={handleSubmit}>
+      <LabelContacts>
+        <span>Name</span>
+        <input
+          type="text"
+          name="name"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+      </LabelContacts>
+      <LabelContacts>
+        <span>Number</span>
+        <input
+          type="tel"
+          name="number"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          value={formData.number}
+          onChange={handleInputChange}
+          required
+        />
+      </LabelContacts>
+      <FormButton type="submit">Add contact</FormButton>
+    </FormContacts>
+  );
+};
