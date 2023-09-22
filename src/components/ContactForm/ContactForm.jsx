191fdc58a936315e932ebe1507d8d8fcea9728a4
YuriyBeregovoy <rik0681@gmail.com>
2023-09-22 23:47:01 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,9 +35,6 @@ export const ContactForm = () => {
         id = {nanoid()} 
         type="text"
         name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"   
-        required
           />
         </LabelContacts>
         <LabelContacts>
@@ -45,8 +42,6 @@ export const ContactForm = () => {
           <input
         type="tel"
         name="number"
-         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"        required
                 />
         </LabelContacts>
        <FormButton type="submit">Add contact</FormButton>
