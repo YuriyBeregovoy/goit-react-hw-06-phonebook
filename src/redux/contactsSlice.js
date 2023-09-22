885d4c6db0c93a2b48c6770256bcba76ee331797
YuriyBeregovoy <rik0681@gmail.com>
2023-09-23 02:10:28 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,17 +1,20 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-
+const contactsInitialState =
+  [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }];
+  
 
 const contactsSlice = createSlice({
   name: "contacts",
-  initialState: {
-    items: [],
-  },
+  initialState: contactsInitialState,
 
   reducers: {
     addContacts: {
       reducer(state, action) {
-        state.items.push(action.payload);
+        state.push(action.payload);
       },
       prepare({name, number}) {
         return {
@@ -24,19 +27,19 @@ const contactsSlice = createSlice({
       }
     },
     deleteContacts(state, action) {
-       const index = state.items.filter(item => item.id !== action.payload);
+       const index = state.filter(item => item.id !== action.payload);
         if (index !== -1) {
         state.items.splice(index, 1);
       }
     },
     updateContact(state, action) {
       const updatedContact = action.payload;
-      const currentContact = state.items.findIndex(
+      const currentContact = state.findIndex(
         contact => contact.id === updatedContact.id
       );
 
       if (currentContact !== -1) {
-        state.items[currentContact] = updatedContact;
+        state[currentContact] = updatedContact;
       }
     },
 
@@ -44,4 +47,4 @@ const contactsSlice = createSlice({
 });
 export const contactsReducer = contactsSlice.reducer;
 export const { addContacts, deleteContacts, updateContact } = contactsSlice.actions;
-export const updatePhonebook = state => state.contacts.items;
+export const updatePhonebook = state => state.contacts;
