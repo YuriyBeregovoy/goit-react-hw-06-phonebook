b48cdfb3cf986253a25010dea6ac75c284f7a4bd
YuriyBeregovoy <rik0681@gmail.com>
2023-09-22 19:00:34 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,10 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const contactsInitialState = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    
 ];
 
 const contactsSlice = createSlice({
@@ -28,7 +25,9 @@ const contactsSlice = createSlice({
     },
     deleteContacts(state, action) {
        const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+        if (index !== -1) {
+        state.splice(index, 1);
+      }
     }
 
   }
