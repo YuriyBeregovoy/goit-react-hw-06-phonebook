6f58938f579111b0488d35d15cf696c0a410b57e
YuriyBeregovoy <rik0681@gmail.com>
2023-09-22 23:33:35 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,16 +1,11 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contactsInitialState = [];
 
 
 const contactsSlice = createSlice({
   name: "contacts",
-  initialState: contactsInitialState,
+  initialState: {contactsInitialState},
 
   reducers: {
     addContacts: {
@@ -36,6 +31,6 @@ const contactsSlice = createSlice({
 
   }
 });
-
+console.log('Початковий стан Redux-стору:', contactsInitialState);
 export const contactsReducer = contactsSlice.reducer;
 export const { addContacts, deleteContacts } = contactsSlice.actions;
