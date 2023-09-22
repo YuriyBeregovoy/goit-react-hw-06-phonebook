af09ccfeb33d6a2b9f15e6c955784b6bf8df2ebf
YuriyBeregovoy <rik0681@gmail.com>
2023-09-22 23:33:30 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,23 +2,29 @@ import { FormButton, FormContacts, LabelContacts } from "./ContactForm.styled"
 import { useDispatch } from "react-redux";
 import { addContacts } from "redux/contactsSlice";
 import { nanoid } from "@reduxjs/toolkit";
-import { store } from "redux/store"
+import { useSelector } from "react-redux";
+
+
+
+
 
 export const ContactForm = () => {
 
   const dispatch = useDispatch();
-
+ const contacts = useSelector((state) => state.contacts);
    const handleNameSet = e => {
     e.preventDefault();
       const { name, number } = e.currentTarget.elements;
      const newContact = { name: name.value, number: number.value, };
-     dispatch(addContacts( newContact));
-     
-        const currentState = store.getState();
-  console.log("Поточний стан Redux-сховища:", currentState);
 
+     const isExistingContact = contacts.map((contact) => contact.name.toLowerCase()).includes(name.value.toLowerCase());
 
-    e.currentTarget.reset();
+  if (!isExistingContact) {
+    dispatch(addContacts(newContact));
+  } else {
+    alert(`${newContact.name} is already in contacts!`);
+  }
+         e.currentTarget.reset();
       }
   
 
@@ -29,8 +35,9 @@ export const ContactForm = () => {
         id = {nanoid()} 
         type="text"
         name="name"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-       
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"   
+        required
           />
         </LabelContacts>
         <LabelContacts>
@@ -38,8 +45,8 @@ export const ContactForm = () => {
           <input
         type="tel"
         name="number"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
+         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"        required
                 />
         </LabelContacts>
        <FormButton type="submit">Add contact</FormButton>
