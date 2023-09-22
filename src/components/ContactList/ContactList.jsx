af9e4dc6bdcd7367de1ff1e8e5c5064a1407831c
YuriyBeregovoy <rik0681@gmail.com>
2023-09-22 14:49:05 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,28 @@
 import { ContactListItem } from "components/ContactListItem/ContactListItem"
 import { List } from "./ContactList.styled"
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { deleteContacts } from "../../redux/contactsSlice";
 
+export const ContactList = () => {
+const dispatch = useDispatch();
 
-export const ContactList = ({ onContactDelete }) => {
 const filter = useSelector((state) => state.filter.filter);
   const contacts = useSelector((state) => state.contacts);
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+
+ const handleContactDelete = (id) => {
+    dispatch(deleteContacts(id));
+  };
+  
+
+ const filteredContacts = contacts.filter((contact) =>
+  typeof contact.name === "string" && contact.name.toLowerCase().includes(filter.toLowerCase())
+);
+
 
   return <List>
     {filteredContacts.map(contact => (
-      <ContactListItem  key={contact.id} contact={contact} onContactDelete={onContactDelete} />
+      <ContactListItem  key={contact.id} contact={contact} onContactDelete={handleContactDelete} />
           ))}
         </List>
 }
